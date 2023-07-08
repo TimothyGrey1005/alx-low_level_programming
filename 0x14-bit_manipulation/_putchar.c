0633087f543d8e102fe41c24d85eb9c3a0e81d2a
OmphileGopane <omphilegopane913@gmail.com>
2023-07-07 18:23:18 -0700

    print binary

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h" 
+#include <unistd.h>
+/**
+* _putchar - character c to stdout
+* @c: The characte
+* Return: success 1.
+* On error, -1 is returned, and errno is set appropriately. */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
