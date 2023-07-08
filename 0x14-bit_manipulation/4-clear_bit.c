7b8403a37fb8ecd01759a7a7e2e8aedebf865411
OmphileGopane <omphilegopane913@gmail.com>
2023-07-07 18:32:33 -0700

    clear bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* clear_bit - set value of a bit to 0
+* @n: point
+* @index: index of the bit to clear *
+* Return: 1 success 0 fail
+*/
+int clear_bit(unsigned long int *n, unsigned int index) {
+if (index > 63) return (-1);
+*n = (~(1UL << index) & *n);
+return (1);
+}
