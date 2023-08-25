b9ada19d45c0907e779ba30860914d759b2eb8e7
OmphileGopane <omphilegopane913@gmail.com>
2023-08-24 22:16:41 -0700

    Updated

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcc -Wall -Wetra -Werror -pendantic -c -fPIC *.c
-gcc -shared -Wl, -soname, liball.so -o liball.so *.o
+gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
